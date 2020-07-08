c247bc63ba003958b3d6039e8e572bd9da974d3f
Abhishek Yenpure <abhishek@uoregon.edu>
2020-07-08 16:29:14 -0700

    adding benchmarking program

--- advection.cxx
+++ advection.cxx
@@ -1,57 +1,41 @@
+#include <chrono>
 #include <vector>
+#include <random>
+
+#include "boost/program_options.hpp"
 
 #include <vapor/Advection.h>
 #include <vapor/DataMgr.h>
 #include <vapor/FileUtils.h>
-#include <vapor/OptionParser.h>
 #include <vapor/VaporField.h>
 
-using namespace Wasp;
 using namespace VAPoR;
 
-struct
+void GenerateSeeds(std::vector<flow::Particle>& seeds,
+                   const std::vector<double>& rake,
+                   const long numOfSeeds)
 {
-  std::string datafile;
-  std::string fieldname;
-  long numberSteps;
-  double stepLength;
-} opt;
-
-OptionParser::OptDescRec_T set_opts[] = {
- {"file" , 1, "filename.nc", "Data file : NetCDF?"},
- {"field" , 1, "(x,y,z)?", "Name of the velocity field"},
- {"steps", 1, "100", "Number of maximum number of steps"},
- {"length", 1, "0.01", "Length of a single step (deltaT)"},
- {nullptr}
-};
-
-OptionParser::Option_T get_options[] = {
-  {"file", Wasp::CvtToCPPStr, &opt.datafile, sizeof(opt.datafile)},
-  {"field", Wasp::CvtToCPPStr, &opt.fieldname, sizeof(opt.fieldname)},
-  {"steps", Wasp::CvtToInt, &opt.numberSteps, sizeof(opt.numberSteps)},
-  {"length", Wasp::CvtToDouble, &opt.stepLength, sizeof(opt.stepLength)},
-  {nullptr}
-};
-
-void InitializeOptions (int argc, char **argv, OptionParser options)
-{
-  std::string ProgName = FileUtils::LegacyBasename(argv[0]);
-  if (options.AppendOptions(set_opts) < 0)
+  std::cout << "[begin] generate seeds" << std::endl;
+  std::cout << "Rake size : " << rake.size() << std::endl;
+  const unsigned int randSeed = 32;
+  std::mt19937 gen(randSeed); //Standard mersenne_twister_engine
+  std::uniform_real_distribution<double> distX( rake.at(0), rake.at(1));
+  std::uniform_real_distribution<double> distY( rake.at(2), rake.at(3));
+  std::uniform_real_distribution<double> distZ( rake.at(4), rake.at(5));
+  const float timeVal = 0.;
+  seeds.resize(numOfSeeds);
+  for( long i = 0; i < numOfSeeds; i++ )
   {
-    std::cerr << ProgName << " : " << options.GetErrMsg();
-    exit(EXIT_FAILURE);
+    seeds[i].location.x = distX(gen);
+    seeds[i].location.y = distY(gen);
+    seeds[i].location.z = distZ(gen);
+    seeds[i].time       = timeVal;
   }
-  if (options.ParseOptions(&argc, argv, get_options) < 0)
-  {
-    std::cerr << ProgName << " : " << options.GetErrMsg();
-    exit(EXIT_FAILURE);
-  }
+  std::cout << "[end] generate seeds" << std::endl;
 }
 
 int main (int argc, char** argv)
 {
-  OptionParser argopts;
-  InitializeOptions(argc, argv, argopts);
   // Input params
   // 1. File
   // 2. Field variable
@@ -59,51 +43,124 @@ int main (int argc, char** argv)
   // 4. Step length (deltaT) : step length is calculated dynamically
   //    This is suggestive?
   // 5. Seeding parameters
+  namespace options = boost::program_options;
+  options::options_description desc("Options");
+  desc.add_options()("data", options::value<std::string>()->required(), "Path to dataset")
+                    ("field", options::value<std::string>()->required(), "Name of vector field")
+                    ("seeds", options::value<long>()->required(), "Number of seed particles")
+                    ("steps", options::value<long>()->required(), "Number of Steps")
+                    ("length", options::value<float>()->required(), "Length of a single step");
+  options::variables_map vm;
+  options::store(options::parse_command_line(argc, argv, desc), vm); // can throw
+  options::notify(vm);
+  if (!(vm.count("data")
+      && vm.count("field")
+      && vm.count("seeds")
+      && vm.count("steps")
+      && vm.count("length")))
+  {
+    std::cout << "Advection Benchmark" << std::endl << desc << std::endl;
+  }
 
+  std::string datafile = vm["data"].as<std::string>();
+  std::string field = vm["field"].as<std::string>();
+  long numSeeds = vm["seeds"].as<long>();
+  long steps = vm["steps"].as<long>();
+  float length = vm["length"].as<float>();
+
+  std::cout << "Advection w/ : "
+            << "\nData : " << datafile
+            << "\nField : " << field
+            << "\nSteps : " << steps
+            << "\nLength : " << length  << std::endl;
+
+  int res;
   // TODO : read field and variable information
   // Let's assume we have read the data and field
-  const std::string fileType = "cf";
-  const size_t cache = 200;
-  const size_t threads = 2;
-
+  const std::string filetype = "cf";
+  const size_t cache = 2000;
+  const size_t threads = 0;
   std::vector<std::string> files;
-  files.push_back(opt.datafile);
-
-  // no options to set that I knowi
-  std::vector<std::string> options;
-  VAPoR::DataMgr datamgr("cf", cache, threads);
-  int res = datamgr.Initialize(files, options);
+  files.push_back(datafile);
+  printf("file = %s\n", files[0].c_str());
+  // no options to set that I know
+  std::vector<std::string> fileopts;
+  //fileopts.push_back("-project_to_pcs");
+  //fileopts.push_back("-vertical_xform");
+  VAPoR::DataMgr datamgr(filetype, cache, threads);
+  res = datamgr.Initialize(files, fileopts);
   if(res < 0)
   {
     std::cerr << "Failed to intialize CF DataMGR" << std::endl;
     exit(EXIT_FAILURE);
   }
-  //VAPoR::FlowParams params;
-
-  std::vector<std::string> variables = datamgr.GetDataVarNames();
-  
-  for(std::vector<std::string>::iterator it = variables.begin() ; it != variables.end(); ++it)
-    std::cout << *it << ',';
-  std::cout << '\n';
-
-/*  //Build a seeding rake
 
   // Create particles
   std::vector<flow::Particle> seeds;
-  // Populate seeds array;
+  std::vector<double> rake;
+  vector<double> mind, maxd;
+  // Get the extents of the dataset
+  // I don't know why this requires a variable name!
+  res = datamgr.GetVariableExtents(0, "uinterp", -1, -1, mind, maxd);
+  if(res < 0)
+  {
+    std::cerr << "Failed to retrieve the extents of data" << std::endl;
+    exit(EXIT_FAILURE);
+  }
+  //Build a seeding rake 
+  rake.push_back(mind.at(0));
+  rake.push_back(maxd.at(0));
+  rake.push_back(mind.at(1));
+  rake.push_back(maxd.at(1));
+  rake.push_back(mind.at(2));
+  rake.push_back(maxd.at(2));
+  // Populate seeds array
+  // Random for now, let users configure later
+  GenerateSeeds(seeds, rake, numSeeds);
+  std::cout << "Will use " << seeds.size() << " seeds." << std::endl;
 
-  flow::Advection advection;
 
-  flow::VaporField velocityField;
+  flow::VaporField velocityField(8);
+  velocityField.IsSteady = true;
   velocityField.AssignDataManager(&datamgr);
+  velocityField.VelocityNames[0] = "uinterp";
+  velocityField.VelocityNames[1] = "vinterp";
+  velocityField.VelocityNames[2] = "winterp";
+
+  ParamsBase::StateSave stateSave;// = nullptr;
+  VAPoR::FlowParams params(&datamgr, &stateSave);
+  params.SetIsSteady(true);
+  params.SetSteadyNumOfSteps(steps);
+  params.SetFlowDirection(static_cast<int>(VAPoR::FlowDir::FORWARD));
+  params.SetSeedGenMode(static_cast<int>(VAPoR::FlowSeedMode::RANDOM));
+  params.SetRandomNumOfSeeds(1000);
   velocityField.UpdateParams(&params);
 
-  int res;
-  res = velocityField.CalcDeltaTFromCurrentTimeStep(deltaT);
-*/
+  res = velocityField.CalcDeltaTFromCurrentTimeStep(length);
+
   // We're building this barebone so no need for field to color with
 
   // Advect particles in the field
+  flow::Advection advection;
+  advection.UseSeedParticles(seeds);
 
+  auto start = chrono::steady_clock::now();
+
+  int advect = flow::ADVECT_HAPPENED;
+  for(size_t step =  advection.GetMaxNumOfPart() - 1;
+      step < steps && advect == flow::ADVECT_HAPPENED; steps++)
+  {
+    advect = advection.AdvectOneStep(&velocityField, length, flow::Advection::ADVECTION_METHOD::RK4);
+    //std::cout << "Advection happened? " << ((advect != 0) ? "yes" : "no") << std::endl;
+  }
+
+  auto end = chrono::steady_clock::now();
+  const double nanotosec = 1e-9;
+  auto elapsed = chrono::duration_cast<chrono::nanoseconds>(end - start).count() * nanotosec;
+  cout << "Elapsed time in nanoseconds : " << elapsed << " sec." << endl;
+
+  /*res = advection.AdvectTillTime(&velocityField, 0, length, 10, flow::Advection::ADVECTION_METHOD::RK4);*/
+  //for(auto& seed : seeds)
+  //  std::cout << seed.location.x << " : " << seed.time << std::endl;
   return 0;
 }
