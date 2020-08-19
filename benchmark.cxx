d16e6b2ba02ec0bbdbaf17f234201d0e6f81ac5e
Abhishek Yenpure <abhishek@uoregon.edu>
2020-08-19 15:27:57 -0700

    Parameters through ini file

--- benchmark.cxx
+++ benchmark.cxx
@@ -30,24 +30,30 @@ int GetFilesFromDirectory(const std::string& datapath,
 }
 
 void GenerateSeeds(std::vector<flow::Particle>& seeds,
-                   const std::vector<double>& rake,
-                   const long numOfSeeds)
+                   const std::vector<double>& xrake,
+                   const std::vector<double>& yrake,
+                   const std::vector<double>& zrake,
+                   const long numOfSeeds,
+                   const double initTime)
 {
   std::cout << "[begin] generate seeds" << std::endl;
-  std::cout << "Rake size : " << rake.size() << std::endl;
   const unsigned int randSeed = 32;
   std::mt19937 gen(randSeed); //Standard mersenne_twister_engine
-  std::uniform_real_distribution<double> distX( rake.at(0), rake.at(1));
-  std::uniform_real_distribution<double> distY( rake.at(2), rake.at(3));
-  std::uniform_real_distribution<double> distZ( rake.at(4), rake.at(5));
-  const float timeVal = 0.;
+  std::uniform_real_distribution<float> distX( xrake.at(0), xrake.at(1));
+  std::uniform_real_distribution<float> distY( yrake.at(0), yrake.at(1));
+  std::uniform_real_distribution<float> distZ( zrake.at(0), zrake.at(1));
   seeds.resize(numOfSeeds);
   for( long i = 0; i < numOfSeeds; i++ )
   {
     seeds[i].location.x = distX(gen);
     seeds[i].location.y = distY(gen);
     seeds[i].location.z = distZ(gen);
-    seeds[i].time       = timeVal;
+    seeds[i].time       = initTime;
+  }
+  for(auto& seed : seeds)
+  {
+    std::cout << "{" << seed.location.x << "," << seed.location.y << ", " << seed.location.y << "} : " << seed.time
+              << std::endl;
   }
   std::cout << "[end] generate seeds" << std::endl;
 }
@@ -69,6 +75,17 @@ int PrintStreams(external::Advection& advector)
   return 0;
 }
 
+void multitoken_double(const std::string& tokens,
+                       std::vector<double>& doubles)
+{
+  const std::string delimiter = " ";
+  size_t pos = 0;
+  pos = tokens.find(delimiter);
+  doubles.push_back(stod(tokens.substr(0, pos)));
+  doubles.push_back(stod(tokens.substr(pos, tokens.size())));
+  std::cout << doubles.size() << std::endl;
+}
+
 int main (int argc, char** argv)
 {
   // Input params
@@ -80,26 +97,41 @@ int main (int argc, char** argv)
   // 5. Seeding parameters
   namespace options = boost::program_options;
   options::options_description desc("Options");
-  desc.add_options()("data", options::value<std::string>()->required(), "Path to dataset")
-                    ("field", options::value<std::string>()->required(), "Name of vector field")
+  desc.add_options()("datapath", options::value<std::string>()->required(), "Path to dataset")
+                    ("fieldx", options::value<std::string>()->required(), "Name of vector field")
+                    ("fieldy", options::value<std::string>()->required(), "Name of vector field")
+                    ("fieldz", options::value<std::string>()->required(), "Name of vector field")
                     ("seeds", options::value<long>()->required(), "Number of seed particles")
                     ("steps", options::value<long>()->required(), "Number of Steps")
-                    ("length", options::value<float>()->required(), "Length of a single step");
+                    ("length", options::value<float>()->required(), "Length of a single step")
+                    ("xrake", options::value<std::string>()->required(), "Number of Steps")
+                    ("yrake", options::value<std::string>()->required(), "Number of Steps")
+                    ("zrake", options::value<std::string>()->required(), "Number of Steps");
   options::variables_map vm;
-  options::store(options::parse_command_line(argc, argv, desc), vm); // can throw
+  std::ifstream settings_file(std::string(argv[1]), std::ifstream::in);
+  options::store(options::parse_config_file(settings_file, desc), vm);
+  settings_file.close();
   options::notify(vm);
-  if (!(vm.count("data")
-      && vm.count("field")
+  if (!(vm.count("datapath")
+      && vm.count("fieldx")
+      && vm.count("fieldy")
+      && vm.count("fieldz")
       && vm.count("seeds")
       && vm.count("steps")
-      && vm.count("length")))
+      && vm.count("length")
+      && vm.count("xrake")
+      && vm.count("yrake")
+      && vm.count("zrake")))
   {
     std::cout << "Advection Benchmark" << std::endl << desc << std::endl;
   }
 
-  std::string datapath = vm["data"].as<std::string>();
+  std::string datapath = vm["datapath"].as<std::string>();
   // Field is not really used
-  std::string field = vm["field"].as<std::string>();
+  std::string fieldx = vm["fieldx"].as<std::string>();
+  std::string fieldy = vm["fieldy"].as<std::string>();
+  std::string fieldz = vm["fieldz"].as<std::string>();
+
   long numSeeds = vm["seeds"].as<long>();
   long steps = vm["steps"].as<long>();
   float length = vm["length"].as<float>();
@@ -107,7 +139,8 @@ int main (int argc, char** argv)
   std::cout << "Available # of threads : " << omp_get_max_threads() << std::endl;
   std::cout << "Advection w/ : "
             << "\nData : " << datapath
-            << "\nField : " << field
+            << "\nField : " << fieldx << "| " << fieldy << " | " << fieldz
+            << "\nSeeds : " << numSeeds
             << "\nSteps : " << steps
             << "\nLength : " << length  << std::endl;
 
@@ -131,11 +164,21 @@ int main (int argc, char** argv)
     std::cerr << "Failed to intialize CF DataMGR" << std::endl;
     exit(EXIT_FAILURE);
   }
-
+  std::vector<double> timeCoords;
+  datamgr.GetTimeCoordinates(timeCoords);
+  double initTime = timeCoords.at(0);
   // Create particles
   std::vector<flow::Particle> seeds;
-  std::vector<double> rake;
-  vector<double> mind, maxd;
+  std::vector<double> xrake;
+  multitoken_double(vm["xrake"].as<std::string>(), xrake);
+  std::cout << "Rake X : [ " << xrake.at(0) << " --- " << xrake.at(1) << "]" << std::endl;
+  std::vector<double> yrake;
+  multitoken_double(vm["yrake"].as<std::string>(), yrake);
+  std::cout << "Rake Y : [ " << yrake.at(0) << " --- " << yrake.at(1) << "]" << std::endl;
+  std::vector<double> zrake;
+  multitoken_double(vm["zrake"].as<std::string>(), zrake);
+  std::cout << "Rake Z : [ " << zrake.at(0) << " --- " << zrake.at(1) << "]" << std::endl;
+  /*vector<double> mind, maxd;
   // Get the extents of the dataset
   // I don't know why this requires a variable name!
   res = datamgr.GetVariableExtents(0, "uinterp", -1, -1, mind, maxd);
@@ -149,26 +192,18 @@ int main (int argc, char** argv)
   {
     std::cerr << "Failed to retrieve the extents of data" << std::endl;
     exit(EXIT_FAILURE);
-  }
-  //Build a seeding rake 
-  rake.push_back(mind.at(0));
-  rake.push_back(maxd.at(0));
-  rake.push_back(mind.at(1));
-  rake.push_back(maxd.at(1));
-  rake.push_back(mind.at(2));
-  rake.push_back(maxd.at(2));
-  // Populate seeds array
+  }*/
+ // Populate seeds array
   // Random for now, let users configure later
-  GenerateSeeds(seeds, rake, numSeeds);
+  GenerateSeeds(seeds, xrake, yrake, zrake, numSeeds, initTime);
   std::cout << "Will use " << seeds.size() << " seeds." << std::endl;
-
 
   flow::VaporField velocityField(8);
   velocityField.IsSteady = true;
   velocityField.AssignDataManager(&datamgr);
-  velocityField.VelocityNames[0] = "uinterp";
-  velocityField.VelocityNames[1] = "vinterp";
-  velocityField.VelocityNames[2] = "winterp";
+  velocityField.VelocityNames[0] = fieldx.c_str();
+  velocityField.VelocityNames[1] = fieldy.c_str();
+  velocityField.VelocityNames[2] = fieldz.c_str();
 
   ParamsBase::StateSave stateSave;// = nullptr;
   VAPoR::FlowParams params(&datamgr, &stateSave);
